4cda311cd86f7b7cbe61de592a79b29cea216afd
BobyDalton <jtepke42@gmail.com>
2012-10-05 17:50:13 +0200

    comments added

--- src/test/StartScreenTest.java
+++ src/test/StartScreenTest.java
@@ -1,13 +1,24 @@
 package test;
 
+import gui.StartScreen;
+
+/**
+ * Class is to test the start screen.
+ * Screen is buggy if GoSco Label was clicked.
+ * Systems which do not support browse desktop action throw Exceptions. 
+ * Therefore a Tooltip was added to GoSco Label which refers to the HTML link.
+ * 
+ * @author jan
+ *
+ */
 public class StartScreenTest {
 
 	public StartScreenTest() {
 		
 	}
-
+	
 	public static void main(String[] args) {		
-
+		StartScreen sc = new StartScreen();
 	}
 
 }
